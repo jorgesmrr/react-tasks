b437059b0af22cb48038aa8f3603d04f0379541f
Jorge Moreira <jorge.smrr@gmail.com>
2021-05-14 04:46:57 -0300

    Use shared master detail component

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,31 +1,52 @@
-import React from 'react';
-import TasksList from '../task/TasksList';
-import ListsList from '../list/ListsList';
-import { connect } from 'react-redux';
+import React from "react";
+import MasterDetail from "@bit/jorgemoreira.headless-react.surface.master-detail";
+import TasksList from "../task/TasksList";
+import ListsList from "../list/ListsList";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showLists: true };
+    }
+
     renderTasks() {
-        return this.props.activeListId
-            ? <TasksList listId={this.props.activeListId} />
-            : <p className="text-neutral-3 p-2">Select a list</p>;
+        return this.props.activeListId ? (
+            <TasksList listId={this.props.activeListId} />
+        ) : (
+            <p className="p-2 text-neutral-3">Select a list</p>
+        );
+    }
+
+    toggleMasterDetail() {
+        this.setState({ showLists: !this.state.showLists });
     }
 
     render() {
         return (
-            <div className="content-medium mt-16">
-                <div className="flex">
-                    <div className="mr-2 w-1/2">
-                        <h1 className="mb-6">Lists</h1>
-                        <ListsList />
-                    </div>
-
-                    <div className="mr-2 w-1/2">
-                        <h1 className="mb-6"> Tasks</h1>
-                        {this.renderTasks()}
-                    </div>
-                </div>
+            <div className="mt-16 content-medium">
+                <MasterDetail
+                    showMaster={this.state.showLists}
+                    onHideMaster={() => this.toggleMasterDetail()}
+                    master={
+                        <div className="h-full p-4 mr-2 bg-white lg:p-0">
+                            <h1 className="mb-6">Lists</h1>
+                            <ListsList />
+                        </div>
+                    }
+                    detail={
+                        <div className="mr-2">
+                            <i
+                                className="cursor-pointer fas fa-bars hover:text-primary-3"
+                                onClick={() => this.toggleMasterDetail()}
+                            />
+                            <h1 className="mb-6">Tasks</h1>
+                            {this.renderTasks()}
+                        </div>
+                    }
+                />
             </div>
-        )
+        );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import './styles/main.output.css';
-import './styles/fontawesome-free-5.13.1-web/css/all.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import "@bit/jorgemoreira.headless.styles";
+import "./styles/main.output.css";
+import "./styles/fontawesome-free-5.13.1-web/css/all.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +14,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
