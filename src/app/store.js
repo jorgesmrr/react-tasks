69df2dc2f728ce0ca5474b250b679603cbc3403f
Jorge Moreira <jorge.smrr@gmail.com>
2021-06-23 00:21:43 -0300

    Persist store state in localStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
-import taskReducer from '../features/task/taskSlice';
-import listReducer from '../features/list/listSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from "../features/task/taskSlice";
+import listReducer from "../features/list/listSlice";
 
-export default configureStore({
-  reducer: {
-    tasks: taskReducer,
-    lists: listReducer
-  },
+const saveToLocalStorage = (state) => {
+    try {
+        localStorage.setItem("todo-state", JSON.stringify(state));
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+const loadFromLocalStorage = () => {
+    try {
+        const stateStr = localStorage.getItem("todo-state");
+        return stateStr ? JSON.parse(stateStr) : undefined;
+    } catch (e) {
+        console.error(e);
+        return undefined;
+    }
+};
+
+const preloadedState = loadFromLocalStorage();
+
+const store = configureStore({
+    reducer: {
+        tasks: taskReducer,
+        lists: listReducer,
+    },
+    preloadedState,
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
